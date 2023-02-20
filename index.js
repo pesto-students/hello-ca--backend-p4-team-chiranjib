6f22ca5c795f67ad6386845f227e4a7be83dec2d
Venugopal <venugopala11000@gmail.com>
2023-02-20 22:46:44 +0530

    start-call api

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const userRouter = require('./src/routes/user.route');
 const callRouter = require('./src/routes/call.route');
 const paymentRouter = require('./src/routes/payment.route');
 const { checkAuth } = require('./src/utils/checkAuth');
+const axios = require('axios');
+const FormData = require("form-data")
 mongoose.set('strictQuery', true);
 
 const corsOptions = {
@@ -38,6 +40,54 @@ app.use('/auth', authRouter);
 app.use('/user', checkAuth, userRouter);
 app.use('/call', callRouter);
 app.use('/payment', checkAuth, paymentRouter);
+
+
+app.post("/start-call", async (req, res) => {
+  try {
+    var data = new FormData();
+    data.append("From", req.body.mobile);
+    data.append("CallerId", "01141133606");
+    data.append("Url", "http://my.exotel.in/exoml/start/590426");
+    data.append("CallType", "trans");
+    data.append("TimeLimit", req.body.timeLimit);
+    data.append(
+      "StatusCallback",
+      "https://9198-2401-4900-1c69-68d0-d9e0-5617-8db7-d764.in.ngrok.io/call/createLogUserCall"
+    );
+    data.append("CustomField", req.body.topics);
+
+    axios
+      .post(
+        "https://api.exotel.com/v1/Accounts/sthambhalliances1/Calls/connect.json",
+        data,
+        {
+          headers: {
+            Authorization:
+              "Basic MDBhNWE4OTk0NmYwZmZmODFhMjAwNGY1NzAxMTY2ZTViMjliMDE1NjQ1ZDI0NjIyOjA2YWU2ZTU0NmJmZDJjZjFjY2I5MGE4Y2FlYjZlMGY3ODZlMjc4MTkwYzczM2ZhOA==",
+          },
+          // auth: {
+          //   username: "RTRIKXNG50AG6RF3OJGAVUWE",
+          //   password: "_TK80jTQr7632CI6aMHsQfkf",
+          // },
+        }
+      )
+      .then((response) => {
+        console.log("response:", response.data);
+        if (response && response.status === 200) {
+          res.send(response.data);
+        } else {
+          res.status(400).send({ message: "something went wrong" });
+        }
+      })
+      .catch((error) => {
+        console.log("error:", error);
+        res.status(400).send({ message: "something went wrong" });
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: "something went wrong" });
+  }
+});
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
